77b3dbf555f00d61286820262ec9ee59327573db
JGamper <jevgenij.gamper5@gmail.com>
2017-03-18 17:36:15 +0000

    Add files via upload

--- recognition.py
+++ recognition.py
@@ -1,46 +1,62 @@
 import speech_recognition as sr
+from time import time
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Do you want a cube?")
-    audio = r.listen(source)
+##### HELPER FUNCTIONS TO CONVERT SPEECH TO TEXT USIGN MSFT BING SPEECH API
 
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    output = r.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-  
-x = "cube" #eventually we put the unity adress for the cube
-if output == "yes":
-    print (x)  #insert cube into VR
+##### MODIFY process for whatever preprocessing you want to do
+def process(output):
+    """
+    Whatever preprocessing step you want to do strings recognized
+    """
 
-else:
-    None 
-    
+    return output
 
-k = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Do you want to strech the cube?")
-    audio = k.listen(source)
+def listenMicrophone():
+    print('I am listening!')
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    return audio
 
 # recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    output = k.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+def convertToText(audio):
+    try:
+        output = r.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
+    except sr.UnknownValueError:
+        print("Microsoft Bing Voice Recognition could not understand audio")
+        output = None
+    except sr.RequestError as e:
+        print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+        output = None
+    return output
 
-output_2 = k.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
+def updateConnectionFile(line, input, file_path):
+    """
+    Updates the file, which is constantly read by unity
+    """
+    with open(file_path, 'w') as f:
+        f.write(input)
 
-if output_2 == "yes":
-    print ("you have streched cube now")
-else:
-    print("you have a cube")
-    
+    return
+
+def listenAndCompute(file_path):
+    while True:
+        audio = listenMicrophone()
+        output = convertToText(audio)
+        if output != None:
+            continue
+        if output:
+            process(output)
+            if output != 'stop':
+                updateConnectionFile(output, file_path)
+
+            else:
+                break
+    return
+
+if __name__ == "__main__":
+    file_path = '.. whatever path you have'
+    BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+    r = sr.Recognizer()
+
+    # function to update the file
+    listenAndCompute(file_path)

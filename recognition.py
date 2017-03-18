2ae82bf332b9dbaeb33ebf49f765f917314e58fd
Taka1996108 <mitsutakashirasu@gmail.com>
2017-03-18 16:05:04 +0000

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -21,5 +21,26 @@ if output == "yes":
 
 else:
     None 
+    
 
+k = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Do you want a cube?")
+    audio = k.listen(source)
 
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+try:
+    output = k.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
+except sr.UnknownValueError:
+    print("Microsoft Bing Voice Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+output_2 = r.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
+
+if output_2 == "yes":
+    print ("you have streched cube now")
+else:
+    print("you have a cube")
+    

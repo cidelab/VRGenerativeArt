322a098ef82f1714067e70f979f9eaa4830cc7ab
cidelab <cidelab@gmail.com>
2017-03-18 19:58:43 +0000

    Unity Project and dont know

--- recognition.py
+++ recognition.py
@@ -1,6 +1,4 @@
 import speech_recognition as sr
-import requests
-from time import time
 
 ##### HELPER FUNCTIONS TO CONVERT SPEECH TO TEXT USIGN MSFT BING SPEECH API
 
@@ -14,12 +12,13 @@ def process(output):
 
 def listenMicrophone():
     print('I am listening!')
-    with sr.Microphone() as source:
+    with sr.Microphone(1) as source:
         audio = r.listen(source)
+        print "done listening"
     return audio
 
 # recognize speech using Microsoft Bing Voice Recognition
-def convertToText(audio):
+def convertToText(audio, r):
     try:
         output = r.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
     except sr.UnknownValueError:
@@ -30,31 +29,35 @@ def convertToText(audio):
         output = None
     return output
 
-def sendRequest(url):
-	r = requests.get(url)
+def updateConnectionFile(output, file_path):
+    """
+    Updates the file, which is constantly read by unity
+    """
+    with open(file_path, 'w') as f:
+        f.write(output)
+        print('wrote into file')
+    return
 
-def sendWords(words):
-	"""
-	Send words to unity, which is constantly read by unity
-	"""
-	print('get words : ' + words)
-	url = 'http://localhost:4000?words=' + words
-	sendRequest(url)
-	return
-
-def listenAndCompute():
+def listenAndCompute(file_path, r):
     while True:
         audio = listenMicrophone()
-        output = convertToText(audio)
-        if output:
+        output = convertToText(audio, r)
+        print(output)
+        if output != None:
+            #process(output)
             if output != 'stop':
-                sendWords(output)
+                print(output)
+                updateConnectionFile(output, file_path)
+
             else:
                 break
     return
 
 if __name__ == "__main__":
+    file_path = 'C:\Users\Carlos\Downloads\VRcommands.txt'
     BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
     r = sr.Recognizer()
+
     # function to update the file
-    listenAndCompute()
+    listenAndCompute(file_path, r)
+

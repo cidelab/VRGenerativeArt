9aff03c97d54fe7282999cf87f66215a3a09078d
Thongchai Wirojsakseree <thongchai.wirojsakseree@tsgforce.com>
2017-03-18 19:00:36 +0000

    add server in python

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
+import requests
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	url = 'http://localhost:4000/test/color?test=aaa&test2=123&test3=aw4'
+	sendRequest(url)
+	return "done"
+
+def sendRequest(url):
+	r = requests.get(url)
+
 
 if __name__ == "__main__":
     app.run()

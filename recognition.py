5540d28f501bc9a98f7a4150c8cedff9b5d19835
JGamper <jevgenij.gamper5@gmail.com>
2017-03-18 15:39:57 +0000

    Add files via upload
    voice rec

---
+++ recognition.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "d6d31a805fca4a9187b7c797fcc50bef" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+try:
+    output = r.recognize_bing(audio, key=BING_KEY, language = "en-US", show_all = False)
+except sr.UnknownValueError:
+    print("Microsoft Bing Voice Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
